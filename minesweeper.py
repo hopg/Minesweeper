11dc843595323e515f88c55616f5980a77ba9af3
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-03 16:15:05 -0400

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -95,102 +95,102 @@ class Mines(Board):
         # Starting at top right corner
         if diff == 1:
             if pos - 1 == 0:
-                the_check(self,[1,3,4], pos)
+                the_check(self, [1, 3, 4], pos)
 
             elif pos - 1 == 1:
-                the_check(self,[0,2,3,4,5], pos)
+                the_check(self, [0, 2, 3, 4, 5], pos)
 
             elif pos - 1 == 2:
-                the_check(self,[1,4,5], pos)
+                the_check(self, [1, 4, 5], pos)
 
             elif pos - 1 == 3:
-                the_check(self,[0,1,4,6,7], pos)
+                the_check(self, [0, 1, 4, 6, 7], pos)
 
             elif pos - 1 == 4:
-                the_check(self,[0,1,2,3,5,6,7,8], pos)
+                the_check(self, [0, 1, 2, 3, 5, 6, 7, 8], pos)
 
             elif pos - 1 == 5:
-                the_check(self,[1,2,4,7,8], pos)
+                the_check(self, [1, 2, 4, 7, 8], pos)
 
             elif pos - 1 == 6:
-                the_check(self,[3,4,7,9,10], pos)
+                the_check(self, [3, 4, 7, 9, 10], pos)
 
             elif pos - 1 == 7:
-                the_check(self,[3,4,5,6,8,9,10,11], pos)
+                the_check(self, [3, 4, 5, 6, 8, 9, 10, 11], pos)
 
             elif pos - 1 == 8:
-                the_check(self,[4,5,7,10,11], pos)
+                the_check(self, [4, 5, 7, 10, 11], pos)
 
             elif pos - 1 == 9:
-                the_check(self,[6,7,10], pos)
+                the_check(self, [6, 7, 10], pos)
 
             elif pos - 1 == 10:
-                the_check(self,[6,7,8,9,11], pos)
+                the_check(self, [6, 7, 8, 9, 11], pos)
 
             elif pos - 1 == 11:
-                the_check(self,[7,8,10], pos)            
+                the_check(self, [7, 8, 10], pos)
 
         elif diff == 2:
             
             if pos - 1 == 0:
-                the_check(self,[1,4,5], pos)
+                the_check(self, [1, 4, 5], pos)
 
             elif pos - 1 == 1:
-                the_check(self,[0,2,4,5,6], pos)
+                the_check(self, [0, 2, 4, 5, 6], pos)
 
             elif pos - 1 == 2:
-                the_check(self,[1,3,5,6,7], pos)
+                the_check(self, [1, 3, 5, 6, 7], pos)
 
             elif pos - 1 == 3:
-                the_check(self,[2,6,7], pos)
+                the_check(self, [2, 6, 7], pos)
 
             elif pos - 1 == 4:
-                the_check(self,[0,1,5,8,9], pos)
+                the_check(self, [0, 1, 5, 8, 9], pos)
 
             elif pos - 1 == 5:
-                the_check(self,[0,1,2,4,6,8,9,10], pos)
+                the_check(self, [0, 1, 2, 4, 6, 8, 9, 10], pos)
 
             elif pos - 1 == 6:
-                the_check(self,[1,2,3,5,7,9,10,11], pos)
+                the_check(self, [1, 2, 3, 5, 7, 9, 10, 11], pos)
 
             elif pos - 1 == 7:
-                the_check(self,[2,3,6,10,11], pos)
+                the_check(self, [2, 3, 6, 10, 11], pos)
 
             elif pos - 1 == 8:
-                the_check(self,[4,5,9,12,13], pos)
+                the_check(self, [4, 5, 9, 12, 13], pos)
 
             elif pos - 1 == 9:
-                the_check(self,[4,5,6,8,10,12,13,14], pos)
+                the_check(self, [4, 5, 6, 8, 10, 12, 13, 14], pos)
 
             elif pos - 1 == 10:
-                the_check(self,[5,6,7,9,11,13,14,15], pos)
+                the_check(self, [5, 6, 7, 9, 11, 13, 14, 15], pos)
 
             elif pos - 1 == 11:
-                the_check(self,[6,7,10,14,15], pos)
+                the_check(self, [6, 7, 10, 14, 15], pos)
                 
             elif pos - 1 == 12:
-                the_check(self,[8,9,13,16,17], pos)  
+                the_check(self, [8, 9, 13, 16, 17], pos)  
                 
             elif pos - 1 == 13:
-                the_check(self,[8,9,10,12,14,16,17,18], pos)
+                the_check(self, [8, 9, 10, 12, 14, 16, 17, 18], pos)
 
             elif pos - 1 == 14:
-                the_check(self,[9,10,11,13,15,17,18,19], pos) 
+                the_check(self, [9, 10, 11, 13, 15, 17, 18, 19], pos) 
                 
             elif pos - 1 == 15:
-                the_check(self,[10,11,14,18,19], pos) 
+                the_check(self, [10, 11, 14, 18, 19], pos) 
                 
             elif pos - 1 == 16:
-                the_check(self,[12,13,17], pos) 
+                the_check(self, [12, 13, 17], pos) 
                 
             elif pos - 1 == 17:
-                the_check(self,[12,13,14,16,18], pos) 
+                the_check(self, [12, 13, 14, 16, 18], pos) 
                 
             elif pos - 1 == 18:
-                the_check(self,[13,14,15,17,19], pos) 
+                the_check(self, [13, 14, 15, 17, 19], pos) 
                 
             elif pos - 1 == 19:
-                the_check(self,[14,15,18], pos)
+                the_check(self, [14, 15, 18], pos)
                 
 def replay():
     global mine_board
